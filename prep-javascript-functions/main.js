4abdb34d77a7e6dc5b05d3e49d797112a081feb5
Laura Grace Manimtim <lauragracem@gmail.com>
2022-04-28 03:07:03 +0000

    prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,20 @@
+/* function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum); */
+
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+var sum = addTwoNumbers(3, 3);
+console.log('sum', sum);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var conversion = convertHoursToMinutes(3);
+console.log(conversion);
